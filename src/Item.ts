37a8802857052dee8342a9b22784a3d99f2be0c5
Isaac Batista <isaacbatst@gmail.com>
2022-09-25 16:39:45 -0300

    add dimensions and weight

--- src/Item.ts
+++ src/Item.ts
@@ -1,5 +1,30 @@
+interface Dimensions {
+	height: number;
+	width: number;
+	depth: number;
+}
+
+interface ItemParams {
+	id: number,
+	description: string,
+	price: number,
+	dimensions: Dimensions,
+	weight: number,
+}
 export default class Item {
+	readonly idItem: number
+	readonly description: string 
+	readonly price: number
+	readonly dimensions: Dimensions
+	readonly weight: number
 	
-	constructor (readonly idItem: number, readonly description: string, readonly price: number) {
+	constructor (
+		params: ItemParams
+	) {
+		this.idItem = params.id;
+		this.description = params.description;
+		this.price = params.price;
+		this.weight = params.weight;
+		this.dimensions = params.dimensions
 	}
 }
--- tests/Order.test.ts
+++ tests/Order.test.ts
@@ -2,6 +2,42 @@ import Coupon from "../src/Coupon";
 import Item from "../src/Item";
 import Order from "../src/Order";
 
+const guitar = {
+	id: 1, 
+	description: 'Guitarra', 
+	dimensions: {
+		depth: 5,
+		height: 100,
+		width: 40
+	},
+	price: 1000,
+	weight: 2
+}
+
+const amp = {
+	id: 2, 
+	description: 'amp', 
+	dimensions: {
+		depth: 5,
+		height: 100,
+		width: 40
+	},
+	price: 5000,
+	weight: 2
+}
+
+const cable = {
+	id: 3, 
+	description: 'cabo', 
+	dimensions: {
+		depth: 5,
+		height: 100,
+		width: 40
+	},
+	price: 30,
+	weight: 5
+}
+
 test("Não deve criar um pedido com CPF inválido", function () {
 	expect(() => new Order("111.111.111-11")).toThrow(new Error("Cpf inválido"));
 });
@@ -14,18 +50,18 @@ test("Deve criar um pedido sem itens", function () {
 
 test("Deve criar um pedido com 3 itens", function () {
 	const order = new Order("317.153.361-86");
-	order.addItem(new Item(1, "Guitarra", 1000), 1);
-	order.addItem(new Item(2, "Amplificador", 5000), 1);
-	order.addItem(new Item(3, "Cabo", 30), 3);
+	order.addItem(new Item(guitar), 1);
+	order.addItem(new Item(amp), 1);
+	order.addItem(new Item(cable), 3);
 	const total = order.getTotal();
 	expect(total).toBe(6090);
 });
 
 test("Deve criar um pedido com 3 itens com cupom de desconto", function () {
 	const order = new Order("317.153.361-86");
-	order.addItem(new Item(1, "Guitarra", 1000), 1);
-	order.addItem(new Item(2, "Amplificador", 5000), 1);
-	order.addItem(new Item(3, "Cabo", 30), 3);
+	order.addItem(new Item(guitar), 1);
+	order.addItem(new Item(amp), 1);
+	order.addItem(new Item(cable), 3);
 	order.addCoupon(new Coupon("VALE20", 20, new Date()));
 	const total = order.getTotal();
 	expect(total).toBe(4872);
@@ -44,14 +80,14 @@ test("Não deve aplicar um cupom de desconto expirado", function () {
 test("Ao fazer um pedido, a quantidade de um item não pode ser negativa", function() {
 	expect(() => {
 		const order = new Order("317.153.361-86");
-		order.addItem(new Item(1, "Guitarra", 1000), -1);
+		order.addItem(new Item(guitar), -1);
 	}).toThrow('Quantidade não pode ser negativa')
 })
 
 test("Ao fazer um pedido, o mesmo item não pode ser informado mais de uma vez", function() {
 	expect(() => {
 		const order = new Order("317.153.361-86");
-		order.addItem(new Item(1, "Guitarra", 1000), 1);
-		order.addItem(new Item(1, "Guitarra", 1000), 1);
+		order.addItem(new Item(guitar), 1);
+		order.addItem(new Item(guitar), 1);
 	}).toThrowError("Item duplicado")
 })
